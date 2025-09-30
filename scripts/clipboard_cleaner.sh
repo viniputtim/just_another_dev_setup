d95cfcb19e4ef8bed562e0b69c0a3347f05b332b
viniputtim <viniputtim@gmail.com>
2025-09-29 22:18:33 -0300

    feat: obsolete scripts removed

--- scripts/clipboard_cleaner.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo -n | xclip -selection clipboard
-echo -n | xclip -selection primary
-qdbus org.kde.klipper /klipper org.kde.klipper.klipper.clearClipboardHistory
--- scripts/github_token_copier.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-7z x /home/vinicius/Desenvolvimento/.secrets/github_token.7z -p"$GITHUB_TOKEN_FILE_PASSWORD"
-cat github_token.txt | tr -d '\n' | xclip -selection clipboard
-rm -rf github_token.txt
